d26488304b0aec40f48df2f283aff80316aa1211
Fabiano <fabiano.ssantos79@gmail.com>
2023-12-27 01:35:03 -0300

    update

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const BotaoEstilizado = styled.button`
-  cursor: pointer;
-  background-color: #eb9b00;
+  background: #eb9b00;
   padding: 16px 32px;
-  color: #ffffff;
-  font-size: 20px;
   border: 2px solid #eb9b00;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
   &:hover {
-    background-color: #b87900;
+    background: #b87900;
     border: 2px solid #b87900;
   }
 `;
 
 export const AbBotao = () => {
-  return <BotaoEstilizado>Clique aqui</BotaoEstilizado>;
+  return <BotaoEstilizado>Clique aqui!</BotaoEstilizado>;
 };
